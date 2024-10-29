5242a191b81fd58ea7664c909dee7feaa3d97e10
Arthur Lauret <arthur.lauret@orange.fr>
2024-10-29 17:05:41 +0100

    Final

--- ArthurLrt.py
+++ ArthurLrt.py
@@ -13,11 +13,13 @@
 # ---
 
 import numpy as np
+import matplotlib.pyplot as plt
+import xcovers as x
 
 
 # +
 def tableau (n,m):
-    T=np.one((n,m))
+    T=np.zeros((n,m))
     return T
 
 def obstacle (tableau,L):
@@ -27,8 +29,31 @@ def obstacle (tableau,L):
         tableau[l][v]==False 
     return tableau
 
+pieces=[[[1, 1, 0], [0, 1, 1], [0, 1, 0]],[[1, 1, 1, 1, 1]],[[1, 0, 0, 0], [1, 1, 1, 1]], [[1, 1, 0, 0], [0, 1, 1, 1]],[[1, 1, 1], [1, 1, 0]],[[1, 1, 1], [0, 1, 0], [0, 1, 0]],[[1, 1, 1], [1, 0, 1]],[[1, 1, 1], [1, 0, 0], [1, 0, 0]],[[1, 0, 0], [1, 1, 0], [0, 1, 1]],[[0, 1, 0], [1, 1, 1], [0, 1, 0]],[[0, 1, 0, 0], [1, 1, 1, 1]],[[1, 1, 0], [0, 1, 0], [0, 1, 1]]]
+dimensions=(6,10)
+# -
 
-    
+
+
+def supprimer_doublons(liste):
+    liste_sans_doublons = []
+    for element in liste:
+        if element not in liste_sans_doublons:
+            liste_sans_doublons.append(element)
+    return liste_sans_doublons
+
+
+def variante(piece):
+    transformations=[piece]
+    transformations.append(np.fliplr(piece).tolist())
+    rotation=piece
+    for i in range (3):
+        rotation = np.rot90(rotation, -1)
+        symétrie = np.fliplr(rotation)
+        transformations.append(rotation.tolist())
+        transformations.append(symétrie.tolist())
+    return supprimer_doublons(transformations)
+
 
 # +
 import numpy as np
@@ -41,7 +66,7 @@ def emplacement(piece, tableau, idx):
     j[idx] = 1
 
     for t in L:
-        (i, j_shape) = t.shape
+        (i, j_shape) = (len(t),len(t[0]))
         for m in range(l - i + 1):
             for n in range(c - j_shape + 1):
                 tableau_prime = tableau.copy()  # Faire une copie pour éviter de modifier le tableau original
@@ -55,12 +80,9 @@ def emplacement(piece, tableau, idx):
     
 # -
 
-t=np.array([[1],[1]])
-piece = [t]
-tableau =np.array([[0,0,0],[0,0,0]])
 
 
-emplacement(piece,tableau,3) 
+
 
 
 # +
@@ -76,6 +98,20 @@ def tableau_final(tableau, pieces):
     
 # -
 
+sol=next(x.covers(matrice))
 
 
+def Tableau_sol(matrice, sol, dimensions):
+    Tableau = np.zeros((dimensions))
+    nombre_de_cases = len(matrice[0]) - len(sol)
+    for indice in sol:
+        Ligne = matrice[indice]
+        for i in range (0, nombre_de_cases - len(sol)):
+            if Ligne[i + len(sol)] == 1: 
+                c = i//dimensions[1]
+                l = i%dimensions[1]
+                Tableau[c, l] = indice
+    plt.imshow(Tableau)
 
+
+Tableau_sol(tableau_final(tableau(dimensions), pieces),sol,dimensions)
