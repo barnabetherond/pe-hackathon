bad87a58c86441b0bd4001446986e08d13a399fe
Arthur Lauret <arthur.lauret@orange.fr>
2024-10-29 14:31:47 +0100

    Arthur

--- ArthurLrt.py
+++ ArthurLrt.py
@@ -1,6 +1,15 @@
-{
- "cells": [],
- "metadata": {},
- "nbformat": 4,
- "nbformat_minor": 5
-}
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.16.4
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+
